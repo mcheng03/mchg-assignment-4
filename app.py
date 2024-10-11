c6b91e6c838dcdbff80ef0f56b421e35ebaeada5
mcheng03 <mcheng@LAPTOP-2SE5CKI5>
2024-10-10 23:18:04 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -5,36 +5,45 @@ import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from numpy.linalg import svd
+import re
+
 
 nltk.download('stopwords')
 
 app = Flask(__name__)
 
-
 newsgroups = fetch_20newsgroups(subset='all')
 documents = newsgroups.data
-stop_words = stopwords.words('english')  
 
-vectorizer = TfidfVectorizer(stop_words=stop_words, max_features=1000)
+
+def clean_document(doc):
+    cleaned = re.sub(r'\W{20,}', ' ', doc)
+    return cleaned
+
+documents = [clean_document(doc) for doc in newsgroups.data]
+
+vectorizer = TfidfVectorizer(stop_words='english', max_features=1000, min_df=4, max_df=0.7, ngram_range=(1, 2))
+
 tfidf_matrix = vectorizer.fit_transform(documents)
 tfidf_matrix = tfidf_matrix.toarray()
+tfidf_mean = np.mean(tfidf_matrix, axis=0)
+tfidf_matrix_centered = tfidf_matrix - tfidf_mean
 
-
-def perform_svd(matrix, k=100):
-    U, S, VT = svd(matrix, full_matrices=False)
+def perform_svd(matrix, k):
+    U, S, Vt = svd(matrix, full_matrices=False)
     U_k = U[:, :k]
     S_k = np.diag(S[:k])
-    VT_k = VT[:k, :]
-    return U_k, S_k, VT_k
+    Vt_k = Vt[:k, :]
+    return U_k, S_k, Vt_k
 
-k = 100  
-U_k, S_k, VT_k = perform_svd(tfidf_matrix, k)
+U_k, S_k, Vt_k = perform_svd(tfidf_matrix_centered, 80)
 
 documents_lsa = np.dot(U_k, S_k)
 
 def preprocess_query(query):
-    query_tfidf = vectorizer.transform([query]).toarray()
-    query_lsa = np.dot(query_tfidf, VT_k.T)
+    query_tfidf = vectorizer.transform([query]).toarray() 
+    query_tfidf_centered = query_tfidf - tfidf_mean
+    query_lsa = np.dot(query_tfidf_centered, Vt_k.T)
     return query_lsa.flatten() 
 
 def cosine_similarity(matrix, vector):
@@ -58,12 +67,8 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
-    try:
-        documents, similarities, indices = search_engine(query)
-        return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices})
-    except Exception as e:
-        app.logger.error(f"Error processing query '{query}': {e}")
-        return jsonify({'error': 'An internal server error occurred'}), 500
+    documents, similarities, indices = search_engine(query)
+    return jsonify({'documents': documents, 'similarities': similarities, 'indices': indices}) 
 
 if __name__ == '__main__':
     app.run(debug=True)
